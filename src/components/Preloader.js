1c3cb8a66bb67902d994e44194e4e065e7b4a0eb
Axel Estrada <axele1524@gmail.com>
2021-02-17 18:28:04 -0600

    Le di estilo y habilite el preloader

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 export default function Preloader() {
+	window.onload = () => {
+		document.querySelector('.preloader').classList.add("hide");
+	};
+
 	return (
-		<div className="preloader">
-			<div className="centrize full-width">
-				<div className="vertical-center">
-					<div className="spinner">
-						<div className="double-bounce1"></div>
-						<div className="double-bounce2"></div>
-					</div>
-				</div>
+		<div className="preloader fixed top-0 left-0 w-full h-full flex justify-center items-center">
+			<div className="spinner relative w-10 h-10 inline-block">
+				<div className="double-bounce1"></div>
+				<div className="double-bounce2"></div>
 			</div>
 		</div>
 	);
