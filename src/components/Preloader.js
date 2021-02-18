0ce1d78d85b732c04387bef2fa667ffe26028753
Axel Estrada <axele1524@gmail.com>
2021-02-18 15:26:44 -0600

    Elimine la referncia a ionicons y mejore el preloader

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function Preloader() {
+	const [preloaderStyle, setPreloaderStyle] = useState({ opacity: '1' });
+
 	window.onload = () => {
-		document.querySelector('.preloader').classList.add("hide");
+		setPreloaderStyle({ opacity: '0' });
 	};
 
 	return (
-		<div className="preloader fixed top-0 left-0 w-full h-full flex justify-center items-center">
+		<div
+			className="preloader fixed top-0 left-0 w-full h-full flex justify-center items-center transition"
+			style={preloaderStyle}
+		>
 			<div className="spinner relative w-10 h-10 inline-block">
 				<div className="double-bounce1"></div>
 				<div className="double-bounce2"></div>
