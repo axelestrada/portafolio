3fa9fd0b2fea29a93ca55cb0f21353a67267d015
Axel Estrada <axele1524@gmail.com>
2021-02-27 16:40:25 -0600

    habilite el formulario de contacto utilizando emailjs

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardContent from './CardContent';
 import Card from './Card';
 
+import { useForm } from 'react-hook-form';
+import emailjs from 'emailjs-com';
+
 export default function ContactCard() {
+	const { register, handleSubmit } = useForm();
+	const [result, setResult] = useState('');
+
+	const onSubmit = (data, e) => {
+		setResult('Sending Message...');
+
+		emailjs.send('service_za4zxg6', 'template_uis5qye', data, 'user_yjvVCwCMVKDiAdqs01kjt').then(
+			(result) => {
+				setResult(result.text === 'OK' ? '¡Message Sent Successfully!' : result.text);
+				e.target.reset();
+
+				setTimeout(() => {
+					setResult('');
+				}, 3000);
+			},
+			(error) => {
+				setResult(error.text);
+			}
+		);
+	};
+
 	return (
 		<Card cardId="contact">
 			<CardContent title="Get in Touch">
@@ -35,15 +59,36 @@ export default function ContactCard() {
 			<CardContent title="Contact Form">
 				<div className="content relative">
 					<div>
-						<form>
+						<form onSubmit={handleSubmit(onSubmit)}>
 							<div className="flex flex-wrap">
-								<FormInput type="text" name="name" placeholder="Full Name" />
-								<FormInput type="email" name="email" placeholder="Email Address" />
-								<FormTextarea name="message" placeholder="Your Message" />
+								<FormInput
+									type="text"
+									name="from_name"
+									placeholder="Full Name"
+									register={register}
+									message="Enter Your Name"
+								/>
+								<FormInput
+									type="email"
+									name="from_email"
+									placeholder="Email Address"
+									register={register}
+									message="Enter Your Email Address"
+								/>
+								<FormTextarea
+									name="message"
+									placeholder="Your Message"
+									register={register}
+									message="Enter Your Message"
+								/>
 							</div>
 							<DownButton text="Send Message" />
 						</form>
 					</div>
+
+					<span className="absolute bottom-5 left-1/2 transform -translate-x-1/2 text-white85 font-normal text-usm">
+						{result}
+					</span>
 				</div>
 			</CardContent>
 		</Card>
@@ -59,7 +104,7 @@ function InfoItem({ title, value }) {
 	);
 }
 
-function FormInput({ name, placeholder, type }) {
+function FormInput({ name, placeholder, type, register, message }) {
 	return (
 		<div className="form-input w-full pt-8 sm:w-1/2">
 			<input
@@ -68,18 +113,30 @@ function FormInput({ name, placeholder, type }) {
 				type={type}
 				name={name}
 				placeholder={placeholder}
+				ref={register({
+					required: {
+						value: true,
+						message: message,
+					},
+				})}
 			/>
 		</div>
 	);
 }
 
-function FormTextarea({ name, placeholder }) {
+function FormTextarea({ name, placeholder, register, message }) {
 	return (
 		<div className="w-full pt-8">
 			<textarea
 				className="w-full resize-none bg-backgroundMain border-b border-white66 text-white85 outline-none text-usm pb-5 focus:border-greenMain hover:border-greenMain"
 				name={name}
 				placeholder={placeholder}
+				ref={register({
+					required: {
+						value: true,
+						message: message,
+					},
+				})}
 			/>
 		</div>
 	);
@@ -87,7 +144,7 @@ function FormTextarea({ name, placeholder }) {
 
 function DownButton({ text }) {
 	return (
-		<button type="submit" className="mt-8 transition text-xs font-medium uppercase text-white85">
+		<button type="submit" className="mt-8 transition text-xs font-medium uppercase outline-none focus:outline-none text-white85">
 			<span>{text}</span>
 			<span className="modern-arrow ml-2"></span>
 		</button>
